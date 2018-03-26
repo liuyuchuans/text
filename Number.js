e4b710bfcfb2f7d552f30257b642f9d353e7dbc2
myisliuyuchuan <37658672+myisliuyuchuan@users.noreply.github.com>
2018-03-26 09:35:25 +0800

    间距

--- Number.js
+++ Number.js
@@ -1,20 +1,20 @@
 var number = 2;
-		var obj = {
-			number : 4,
-			fn1 : (function(){
-				this.number *= 2;
-				number = number * 2;
-				var number = 3;
-				return function (){
-					this.number *= 2;
-					number *=3;
-					alert(number);
-				}
-			})();
-		};
-		var fn1 = obj.fn1;
-		alert(number);   
-		fn1();         
-		obj.fn1();      
-		alert(window.number);
-		alert(obj.number);  
+var obj = {
+	number : 4,
+	fn1 : (function(){
+		this.number *= 2;
+		number = number * 2;
+		var number = 3;
+		return function (){
+			this.number *= 2;
+			number *=3;
+			alert(number);
+		}
+	})();
+};
+var fn1 = obj.fn1;
+alert(number);   
+fn1();         
+obj.fn1();      
+alert(window.number);
+alert(obj.number);  
